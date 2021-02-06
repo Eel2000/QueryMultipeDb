737db347dae29df76d130b84c87b4d921216321a
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2021-02-06 10:01:52 +0100

    Ajoutez des fichiers projet.

---
+++ MultiDbQuery/AvailableContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiDbQuery.Errors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace MultiDbQuery
+{
+    public static class AvailableContext
+    {
+        public static int propertiesNumber { get; set; }
+        private static PropertyInfo[] propertyInfo { get; set; }
+        private static List<DbContext> _dbContexts { get; set; } = new List<DbContext>();
+
+        public static IEnumerable<DbContext> GetContexts<TObject>(TObject @object)
+        {
+            propertyInfo = @object.GetType().GetProperties();
+            propertiesNumber = propertyInfo.Length;
+
+            if (propertiesNumber < 0)
+                throw new ReadPropertiesException();
+
+            foreach (var item in propertyInfo)
+            {
+                if (item.Name.Contains("Context") || item.Name.Contains("db") ||
+                    item.Name.Contains("context"))
+                {
+
+                    var context = @object.GetType()
+                        .GetProperties()
+                        .Where(x => x.Name == item.Name)
+                        .FirstOrDefault();
+
+                    var c = (Object)context?.GetValue(@object);
+
+                    _dbContexts.Add((DbContext)c);
+                }
+
+            }
+
+            return _dbContexts;
+        }
+    }
+}
---
+++ MultiDbQuery/Errors/ReadPropertiesException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiDbQuery.Errors
+{
+    public class ReadPropertiesException : Exception
+    {
+        public ReadPropertiesException()
+        {
+            throw new ReadPropertiesException("Error occur while readind class properties.\r" +
+                "It's seems like this class does not contains any property.");
+
+        }
+
+        public ReadPropertiesException(string message):base(message)
+        {
+
+        }
+    }
+}
